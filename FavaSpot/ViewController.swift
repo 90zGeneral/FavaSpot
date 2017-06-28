7923ea13a41cede360e9e11093cdf6c32473ec66
Roydon Jeffrey <brushdam14823@gmail.com>
2017-06-27 23:27:47 -0400

    Attempting to add the user's location to the map after the user navigates away from the list of their favorite spots in the RootVC but there is an error currently needed to be fixed. Very minor and annoying but no big deal

--- FavaSpot/ViewController.swift
+++ FavaSpot/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    //Outlet
+    @IBOutlet weak var mapView: MKMapView!
+   
+    //Location Manager
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //Delegates
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        //Permission request for user's location
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        
+        let userLat: CLLocationDegrees = userLocation.coordinate.longitude
+        let userLong: CLLocationDegrees = userLocation.coordinate.longitude
+        let userLatDelta: CLLocationDegrees = 0.01
+        let userLongDelta: CLLocationDegrees = 0.01
+        
+        let userCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: userLat, longitude: userLong)
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: userLatDelta, longitudeDelta: userLongDelta)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: userCoordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        //Create an annotation
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = userCoordinate
+        annotation.title = "Current Location"
+        annotation.subtitle = "You are here now"
+        mapView.addAnnotation(annotation)
+    }
 
 }
 
