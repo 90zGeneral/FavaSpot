12ea0569e728eb662524fa5187a421d3be41f847
Roydon Jeffrey <brushdam14823@gmail.com>
2017-06-28 21:24:58 -0400

    Fixed the silly little mistake. Accidentally used the longitude property to represent both latitude and longitude of the user's location.

--- FavaSpot/ViewController.swift
+++ FavaSpot/ViewController.swift
@@ -10,43 +10,34 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     //Outlet
     @IBOutlet weak var mapView: MKMapView!
    
     //Location Manager
-    var locationManager = CLLocationManager()
+    var locationManager: CLLocationManager!
+    
+    var userCoordinate = CLLocationCoordinate2D()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Initializer
+        locationManager = CLLocationManager()
         
         //Delegates
         mapView.delegate = self
         locationManager.delegate = self
         
         //Permission request for user's location
-        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
 
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        let userLocation: CLLocation = locations[0]
-        
-        let userLat: CLLocationDegrees = userLocation.coordinate.longitude
-        let userLong: CLLocationDegrees = userLocation.coordinate.longitude
-        let userLatDelta: CLLocationDegrees = 0.01
-        let userLongDelta: CLLocationDegrees = 0.01
-        
-        let userCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: userLat, longitude: userLong)
-        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: userLatDelta, longitudeDelta: userLongDelta)
-        
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: userCoordinate, span: span)
-        
-        mapView.setRegion(region, animated: true)
+    override func viewDidAppear(_ animated: Bool) {
         
         //Create an annotation
         let annotation = MKPointAnnotation()
@@ -54,6 +45,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         annotation.title = "Current Location"
         annotation.subtitle = "You are here now"
         mapView.addAnnotation(annotation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        print(userLocation)
+        
+        let userLat: CLLocationDegrees = userLocation.coordinate.latitude
+        let userLong: CLLocationDegrees = userLocation.coordinate.longitude
+        let userLatDelta: CLLocationDegrees = 0.01
+        let userLongDelta: CLLocationDegrees = 0.01
+
+        userCoordinate = CLLocationCoordinate2D(latitude: userLat, longitude: userLong)
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: userLatDelta, longitudeDelta: userLongDelta)
+
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: userCoordinate, span: span)
+
+        mapView.setRegion(region, animated: false)
+        
     }
 
 }
